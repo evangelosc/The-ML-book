b7958333a4de9cba3bf380317e32f93915b5727c
Evangelos Charalampidis <evan.charalampidis@gmail.com>
2020-08-23 13:52:24 +0100

    example 2 - no depth explanation

--- Aurelien-Geron/chapter2/example2.py
+++ Aurelien-Geron/chapter2/example2.py
@@ -57,3 +57,50 @@ housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
 train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
 
 test_set.head()
+
+from sklearn.model_selection import train_test_split
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+test_set.head()
+
+# hosuing["median_income"].hist()
+
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+
+housing["income_cat"].value_counts()
+
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    print(train_index, test_index)
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+strat_test_set["income_cat"].value_counts() / len(strat_test_set)
+
+housing["income_cat"].value_counts() / len(housing)
+
+
+def income_cat_proportions(data):
+    return data["income_cat"].value_counts() / len(data)
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+compare_props = pd.DataFrame({
+    "Overall": income_cat_proportions(housing),
+    "Stratified": income_cat_proportions(strat_test_set),
+    "Random": income_cat_proportions(test_set),
+}).sort_index()
+compare_props["Rand. %error"] = 100 * compare_props["Random"] / compare_props["Overall"] - 100
+compare_props["Strat. %error"] = 100 * compare_props["Stratified"] / compare_props["Overall"] - 100
+
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+housing = strat_train_set.copy()
